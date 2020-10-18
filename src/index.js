4538a1ea88b5dd18014ad88db8095aa408449170
Jason Gu <Jason_Gu@epam.com>
2020-10-18 19:58:39 +0800

    feat: add hot reload feature

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { ErrorBoundary } from './components';
-import Routes from './routes'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <Routes />
-      </ErrorBoundary>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = () => {
+  const Routes = require('./routes').default;
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <Routes />
+        </ErrorBoundary>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
+if (module.hot) {
+  module.hot.accept('./routes/index', () => {
+    renderApp();
+  });
+}
+renderApp();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
