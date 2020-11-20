3cdd02c8c3d404293e0dffab5d2d103959048523
Jason Gu <Jason_Gu@epam.com>
2020-11-20 14:55:31 +0800

    build: use thread-loader to reduce the buliding time

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,17 @@ module.exports = override(
   addBundleVisualizer({
     analyzerMode: 'static',
     reportFilename: 'report.html',
-  },true),
+  }, true),
+  (config) => {
+    const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+    const jsLoader = loaders[1];
+    jsLoader.use =
+      [
+        require.resolve('thread-loader'),
+        { loader: jsLoader.loader, options: jsLoader.options }
+      ];
+    delete jsLoader.loader;
+    delete jsLoader.options;
+    return config;
+  }
 );
